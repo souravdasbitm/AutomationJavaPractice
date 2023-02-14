ab70d9de821ac7856d188a9c982ac72da502054f
Sourav Das <souravdasbitm@gmail.com>
2023-02-14 10:20:46 +0530

    Added array

--- src/main/java/RunnerClass.java
+++ src/main/java/RunnerClass.java
@@ -3,27 +3,27 @@ import java.util.Arrays;
 public class RunnerClass {
 
     public static void main(String[] args) {
-        ParentClass my = new ParentClass();
-
-        my.sum(1, 2);
-        my.sum(1, 2, 3, "This");
-        my.minus(1, 2);
-
-        ParentClass.division(4, 2);
-
-        ChildClass child = new ChildClass();
-
-        child.multiple(1, 2);
-        child.sum(1, 2);
-
-        child.overriddenMethod();
-        child.useOfParentMethod(8, 9);
-
-        child.parentChildOverrideMethod();
-
-        StudentConstructor stuc = new StudentConstructor("Vinod", 1);
-
-        stuc.display();
+//        ParentClass my = new ParentClass();
+//
+//        my.sum(1, 2);
+//        my.sum(1, 2, 3, "This");
+//        my.minus(1, 2);
+//
+//        ParentClass.division(4, 2);
+//
+//        ChildClass child = new ChildClass();
+//
+//        child.multiple(1, 2);
+//        child.sum(1, 2);
+//
+//        child.overriddenMethod();
+//        child.useOfParentMethod(8, 9);
+//
+//        child.parentChildOverrideMethod();
+//
+//        StudentConstructor stuc = new StudentConstructor("Vinod", 1);
+//
+//        stuc.display();
 
         int[] arr = {4, 7, 5, 8};
 
@@ -51,6 +51,6 @@ public class RunnerClass {
         // array = 7,8,5,4,9,10
         int[] second = {7, 8, 5, 4, 0, 10};
         Arrays.sort(second);
-        System.out.println("This is second highest value of the array--> " + second[second.length - 1]);
+        System.out.println("This is second highest value of the array--> " + second[second.length - 2]);
     }
 }
